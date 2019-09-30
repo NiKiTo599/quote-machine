6c06d5f92a3dc5a5e89ce29d6a8a776e46cfd2b9
NiKiTo599 <akulichnikita0@gmail.com>
2019-09-30 12:37:25 +0300

    Updates

---
+++ precache-manifest.9dba5c9c18cd58b5b223014b44c657ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d6700e8597b052d0e77c778fa4f0232",
+    "url": "/FreeCodeCampProjects/quote-machine/index.html"
+  },
+  {
+    "revision": "be473eb228b294282bfe",
+    "url": "/FreeCodeCampProjects/quote-machine/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "90ef12b425daca42c6c5",
+    "url": "/FreeCodeCampProjects/quote-machine/static/js/2.6b819420.chunk.js"
+  },
+  {
+    "revision": "be473eb228b294282bfe",
+    "url": "/FreeCodeCampProjects/quote-machine/static/js/main.bae27faa.chunk.js"
+  },
+  {
+    "revision": "0a0eaae5cc3e50f40d27",
+    "url": "/FreeCodeCampProjects/quote-machine/static/js/runtime-main.47a9ba32.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/FreeCodeCampProjects/quote-machine/static/media/logo.25bf045c.svg"
+  }
+]);
